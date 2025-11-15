b18d5e3007d0b9818677694b53967c488e64883c
nadavzamir2 <nadavzamir2@gmail.com>
2025-11-15 23:18:37 +0200

    form

--- app/src/pages/AddVacationPage.tsx
+++ app/src/pages/AddVacationPage.tsx
@@ -7,17 +7,24 @@ import { createVacation } from "@/services/createVacation";
 
 
 export const AddVacationPage = () => {
-  const [vacation, setVacation] = useState({
-    destination: "",
-    description: "",
-    startDate: "",
-    endDate: "",
-    price: "",
-    image: null as File | null,
-  });
+  const [firstName, setFirstName] = useState("");
+  const [firstNameError, setFirstNameError] = useState(null as string | null);
+  useEffect(() => {
+    if (firstName === "nadav") {
+      setFirstNameError("Nadav is the king!")
+    } else {
+      setFirstNameError(null);
+    }
+  }, [firstName])
   return (
     <div className="add-vacation-page">
-      <h1>Add Vacation</h1>
+      <h1>Add Vacation {firstName}</h1>
+      <form>
+        <label>First Name
+        <input type="text" value={firstName} onChange={(e) => { setFirstName(e.target.value) }}></input>
+        </label>
+        {firstNameError && (<div>{firstNameError}</div>)}
+      </form>
     </div>
   )
 }
